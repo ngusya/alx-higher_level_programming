56eb0d1f6f8b98f4a65fd6612fbe560ac2cc3c5c
ngusya <ngusyakelvin@gmail.com>
2021-12-16 02:47:19 +0300

    Update task 3

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,10 +1,9 @@
 #!/usr/bin/node
-if(process.argv.length === 2){
-    console.log('No argument');
-}
-if (process.argv.length === 3){
-    console.log('Argument found')
-}
-if(process.argv.length > 3){
-    console.log('Arguments found')
+const argv = process.argv;
+if (argv.length < 3) {
+  console.log('No argument');
+} else if (argv.length === 3) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
 }
