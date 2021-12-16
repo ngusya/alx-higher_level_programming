42fe7dfc2391af4658128a21df6a802de93bcc68
ngusya <ngusyakelvin@gmail.com>
2021-12-16 12:57:30 +0300

    Update task 7

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,6 @@
 #!/usr/bin/node
-const arg = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-for (let i = 0; i < 3; i++) {
-  console.log(arg[i]);
+// iterate list
+const array = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+for (let step = 0; step < array.length; step++) {
+  console.log(array[step]);
 }
