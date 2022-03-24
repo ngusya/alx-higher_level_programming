96b8b3853b7623c7cdb33038aead021c1ba78e7e
ngusya <ngusyakelvin@gmail.com>
2022-03-24 23:15:57 +0300

    Update 0x03 0x04 0x05

---
+++ 0x03-python-data_structures/Python.h
@@ -0,0 +1,9 @@
+#ifdefine Python.h
+#define Python.h
+ /*
+  *Description: singly linked list node structure
+  *for Holberton project
+  *
+  */
+void print_python_list_info(PyObject *p);
+#endif /* Python */
