07ad5aab62ddb02a18a5ff118c4898cf24d45486
ngusya <ngusyakelvin@gmail.com>
2022-03-27 00:17:11 +0300

    Task 8 Fix

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,24 +1,19 @@
 #!/usr/bin/python3
-"""
-takes in a letter and sends a POST request to http://0.0.0.0:5000/search_user
-with the letter as a parameter
-"""
-if __name__ == '__main__':
+"""Takes in a letter and sends a POST request to
+http://0.0.0.0:5000/search_user with the letter as a parameter"""
+
+
+if __name__ == "__main__":
     import requests
-    from sys import argv
-    if len(argv) == 2:
-        q = argv[1]
-    else:
-        q = ""
+    import sys
+
+    q = sys.argv[1] if len(sys.argv) > 1 else ""
     r = requests.post('http://0.0.0.0:5000/search_user', data={'q': q})
     try:
-        r_dict = r.json()
-        id = r_dict.get('id')
-        name = r_dict.get('name')
-        if len(r_dict) == 0 or not id or not name:
-            print("No result")
+        dic = r.json()
+        if dic == {}:
+            print('No result')
         else:
-            print("[{}] {}".format(r_dict.get('id'), r_dict.get('name')))
-    except:
-        print("Not a valid JSON")
-
+            print("[{}] {}".format(dic.get('id'), dic.get('name')))
+    except ValueError:
+        print('Not a valid JSON')
