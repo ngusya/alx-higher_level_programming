29b0a83f250c224cd6531f704c86cbc21a9d73fa
ngusya <ngusyakelvin@gmail.com>
2022-03-27 00:29:11 +0300

    Task 6 Fix

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,17 +1,12 @@
 #!/usr/bin/python3
-# Python script that takes in a URL and an email address, sends a POST
-#  request to the passed URL with the email as a parameter, and finally
-#  displays the body of the response.
-#     The email must be sent in the variable email
-#     You must use the packages requests and sys
-#     You are not allowed to import packages other than requests and sys
-#     You don’t need to error check arguments passed to the script
-#       (number or type)
+"""Takes in a URL and an email address,
+sends a POST request to the passed URL with the email as a parameter,
+and finally displays the body of the response"""
 
-import requests
-import sys
 
 if __name__ == "__main__":
-    payload = {'email': sys.argv[2]}
-    r = requests.post(sys.argv[1], data=payload)
+    import requests
+    import sys
+
+    r = requests.post(sys.argv[1], data={'email': sys.argv[2]})
     print(r.text)
