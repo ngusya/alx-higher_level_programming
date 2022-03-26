c5105deea5b0b9b75c9ec718f5dc9de660d13adf
ngusya <ngusyakelvin@gmail.com>
2022-03-26 15:48:09 +0300

    Task 4 fix

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python3
+
 """
-Contains the inherits_from function
+module with method inherits_from
+
 """
 
+
 def inherits_from(obj, a_class):
-    """returns true if obj is a subclass of a_class, otherwise false"""
-    return(issubclass(type(obj), a_class) and type(obj) != a_class)
+    """Method that return True if an object is an instance of a class
+    that inherited from"""
+
+    return False if type(obj) is a_class else isinstance(obj, a_class)
