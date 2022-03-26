8d36bf552edfae42b07f3ec391b1b170aa70d66d
ngusya <ngusyakelvin@gmail.com>
2022-03-26 23:02:43 +0300

    Task 8 fix

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,18 +1,11 @@
 #!/usr/bin/python3
-"""Module 10-class_to_json.
-Returns the dictionary description with
-simple data structure (list, dictionary,
-string, integer and boolean)
-for JSON serialization of an object.
+"""
+Contains the "class_to_json" function
 """
 
 
 def class_to_json(obj):
-    """Creates a dict description of obj.
-    Args:
-        - obj: object to serialize
-    Returns: dictionnary description of obj
-    """
-
+    """returns the dictionary description with simple data structure
+    (list, dictionary, string, integer and boolean)
+    for JSON serialization of an object"""
     return obj.__dict__
-
