1f110e0bb5832d3abc489db72e868810580fa1e7
Ubuntu <ubuntu@Ubuntu.myguest.virtualbox.org>
2023-12-21 22:05:59 +0300

    Fixed 2-square.py

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,19 +1,18 @@
 #!/usr/bin/python3
-
-"""Defines a class square"""
+""" class Square that defines a square"""
 
 
 class Square:
-    """"Defines a new square"""
+    """ class Square that defines a square"""
 
-    
     def __init__(self, size=0):
-        """Initializes the square
+        """initialize square
         Args:
-         size(int): Size of the square
-         """
-        if not isinstance(self.__size, int):
-            raise TypeError("size must be an integer")
-        elif self.__size < 0:
-            raise ValueError("size must be >= 0")
-        self.__size = size
+            size (int): size of the square
+        """
+        if type(size) is not int:
+            raise TypeError('size must be an integer')
+        elif size < 0:
+            raise ValueError('size must be >= 0')
+        else:
+            self.__size = size  #: size of the square
