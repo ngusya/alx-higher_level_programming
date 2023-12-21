0d475a6d4ae8b2067819202d903a301e12f1197b
Ubuntu <ubuntu@Ubuntu.myguest.virtualbox.org>
2023-12-21 22:27:41 +0300

    Fixed 5-square.py pycode style

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 """Defines a class Square"""
 
+
 class Square:
 
-    
+  
     """
     Class that defines properties of square by: (based on 4-square.py).
-
     Attributes:
         size: size of a square (1 side).
     """
